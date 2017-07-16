8cce21b68d52c728a8348674deff8c03d5fb74b7
ruchika <ruchikagarg764@gmail.com>
2017-07-16 19:45:23 +0530

    full project submission

--- instabot.py
+++ instabot.py
@@ -1,6 +1,7 @@
 import requests,urllib
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
+from termcolor import colored
 
 
 APP_ACCESS_TOKEN = '5698649413.276436a.79e5fcd89001466eacb46433ade04899'
@@ -20,9 +21,9 @@ def self_info():
             print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
             print 'No. of posts: %s' % (user_info['data']['counts']['media'])
         else:
-            print 'User does not exist!'
+            print (colored('User does not exist!','red'))
     else:
-        print 'Status code other than 200 received!'
+        print (colored('Status code other than 200 received!','red'))
 
 #to get id of user by username declare a function.
 
@@ -37,14 +38,14 @@ def get_user_id(insta_username):
         else:
             return None
     else:
-        print 'Status code other than 200 received!'
+        print (colored('Status code other than 200 received!','red'))
         exit()
 #function declaration o get info of a user by username.
 
 def get_user_info(insta_username):
     user_id = get_user_id(insta_username)
     if user_id == None:
-        print 'User does not exist!'
+        print (colored('User does not exist!','red'))
         exit()
     request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -57,9 +58,9 @@ def get_user_info(insta_username):
             print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
             print 'No. of posts: %s' % (user_info['data']['counts']['media'])
         else:
-            print 'There is no data for this user!'
+            print (colored('There is no data for this user!','red'))
     else:
-        print 'Status code other than 200 received!'
+        print (colored('Status code other than 200 received!','red'))
 
 #function declaration to get your own recent post.
 
@@ -75,16 +76,16 @@ def get_own_post():
             urllib.urlretrieve(image_url, image_name)
             print 'Your image has been downloaded!'
         else:
-            print 'Post does not exist!'
+            print (colored('Post does not exist!','red'))
     else:
-        print 'Status code other than 200 received!'
+        print (colored('Status code other than 200 received!','red'))
 
 #function declaration to get the recent post of a user by user name.
 
 def get_user_post(insta_username):
     user_id = get_user_id(insta_username)
     if user_id == None:
-        print 'User does not exist!'
+        print (colored('User does not exist!','red'))
         exit()
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -95,18 +96,18 @@ def get_user_post(insta_username):
             image_name = user_media['data'][0]['id'] + '.jpeg'
             image_url = user_media['data'][0]['images']['standard_resolution']['url']
             urllib.urlretrieve(image_url, image_name)
-            print 'Your image has been downloaded!'
+            print (colored('Your image has been downloaded!','green'))
         else:
-            print 'Post does not exist!'
+            print (colored('Post does not exist!','red'))
     else:
-        print 'Status code other than 200 received!'
+        print (colored('Status code other than 200 received!','red'))
 
 #function declaration to get id of recent post of a user by username.
 
 def get_post_id(insta_username):
     user_id = get_user_id(insta_username)
     if user_id == None:
-        print 'User does not exist!'
+        print (colored('User does not exist!','red'))
         exit()
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -116,10 +117,10 @@ def get_post_id(insta_username):
         if len(user_media['data']):
             return user_media['data'][0]['id']
         else:
-            print 'There is no recent post of the user!'
+            print (colored('There is no recent post of the user!','red'))
             exit()
     else:
-        print 'Status code other than 200 received!'
+        print (colored('Status code other than 200 received!','red'))
         exit()
 
 #function declaration to like the recent post of a user.
@@ -131,9 +132,31 @@ def like_a_post(insta_username):
     print 'POST request url : %s' % (request_url)
     post_a_like = requests.post(request_url, payload).json()
     if post_a_like['meta']['code'] == 200:
-        print 'Like was successful!'
+        print (colored('Like was successful!','green'))
     else:
-        print 'Your like was unsuccessful. Try again!'
+        print (colored('Your like was unsuccessful. Try again!','red'))
+
+
+#function declaration to get the recent media like by user
+
+def recent_media_liked():
+    #media_id=get_post_id(insta_username)
+    request_url = (BASE_URL + "users/self/media/liked?access_token=%s") % (APP_ACCESS_TOKEN)
+    print 'GET request url:%s' % (request_url)
+    liked_media = requests.get(request_url).json()
+    if (liked_media['meta']['code'] == 200):
+        if len(liked_media['data']):
+            image_name=liked_media['data'][0]['id']+'.jpeg'
+            image_url=liked_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url,image_name)
+            print(colored("your image has been downloaded.",'green'))
+            return liked_media['data'][0]['id']
+        else:
+            print (colored("there is no recent post.",'red'))
+    else:
+        print (colored("status code other than 200 received!",'red'))
+    return None
+
 
 # function declaration to get the list of user who like posts
 def get_like_list(insta_username):
@@ -146,9 +169,9 @@ def get_like_list(insta_username):
             for a in range(0, len(like_list['data'])):
              print like_list['data'][a]['username']
         else:
-         print"like doesn't exsist."
+         print(colored("like doesn't exsist.",'red'))
     else:
-     print"status code other than 200."
+     print(colored("status code other than 200.",'red'))
 
 
 #function declaration to make the comment on the recent post of the user.
@@ -163,9 +186,9 @@ def post_a_comment(insta_username):
     make_comment = requests.post(request_url, payload).json()
 
     if make_comment['meta']['code'] == 200:
-        print "Successfully added a new comment!"
+        print(colored("Successfully added a new comment!",'green'))
     else:
-        print "Unable to add comment. Try again!"
+        print (colored("Unable to add comment. Try again!",'red'))
 
 
 #function declaration to get the comment list.
@@ -181,9 +204,9 @@ def get_comment_list(insta_username):
                 print comments_list['data'][a]['from']['username']+':',
                 print comments_list['data'][a]['text']
         else:
-            print"comment doesn't exsist."
+            print(colored("comment doesn't exsist."))
     else:
-        print"status code other than 200."
+        print(colored("status code other than 200.",'red'))
 
 
 #function declaration to make delete negative comments from the recent post of the other users.
@@ -208,15 +231,79 @@ def delete_negative_comment(insta_username):
                     delete_info = requests.delete(delete_url).json()
 
                     if delete_info['meta']['code'] == 200:
-                        print 'Comment successfully deleted!\n'
+                        print (colored('Comment successfully deleted!\n','green'))
                     else:
-                        print 'Unable to delete comment!'
+                        print (colored('Unable to delete comment!','red'))
                 else:
                     print 'Positive comment : %s\n' % (comment_text)
         else:
-            print 'There are no existing comments on the post!'
+            print (colored('There are no existing comments on the post!','red'))
+    else:
+        print (colored('Status code other than 200 received!','red'))
+
+
+
+#function declaration to iterate through the negative comments on posts.
+
+def iterate_through_negative_comments(media_id):
+    request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    comment_info = requests.get(request_url).json()
+    if comment_info['meta']['code']==200:
+        if len(comment_info['data']):
+            for i in range(0,len(comment_info['data'])):
+                comment_id=comment_info['data'][i]['id']
+                comment_text=comment_info['data'][i]['text']
+                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
+                if (blob.sentiment.p_neg > blob.sentiment.p_pos):
+                    print 'Negative comment : %s' % (comment_text)
+        else:
+            print (colored('There are no existing comments on the post!','red'))
+    else:
+        print (colored('Status code other than 200 received!','red'))
+
+
+#   function declaration to get minimum likes
+
+
+def min_likes_on_post(insta_username):
+    user_id=get_user_id(insta_username)
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:                                                             # check for status code of the request
+        if len(user_media['data']):
+            like=[]
+            i=0
+            while(i<len(user_media['data'])):
+                likes_on_post=user_media['data'][i]['likes']['count']
+                like.append(likes_on_post)
+                i=i+1
+            min_like=min(like)
+            j=0
+            while(j<len(like)):
+               if(like[j]==min_like):
+                    image_name = user_media['data'][j]['id'] + '.jpeg'
+                    image_url = user_media['data'][j]['images']['standard_resolution']['url']
+                    urllib.urlretrieve(image_url, image_name)                                               # image url is retrieved
+                    print 'GET image_url is :%s' % (image_url)
+                    print 'Your image has been downloaded! By clicking above link you can view you the image with minimum number of likes'
+               j=j+1
+        else:
+            print 'This account has zero post'
+            exit()
+
     else:
         print 'Status code other than 200 received!'
+        exit()
+
+
+
+
+
+
+
 
 #put your choice,what do you want to.
 
@@ -225,17 +312,24 @@ def start_bot():
         print '\n'
         print 'Hey! Welcome to instaBot!'
         print 'Here are your menu options:'
+        print 'My sandbox users are:'
+        print '1. astha_rc_\n'
+        print '2. itzz_mehakk\n'
+        print '3. selfie_queen.__\n'
         print "a.Get your own details\n"
         print "b.Get details of a user by username\n"
         print "c.Get your own recent post\n"
         print "d.Get the recent post of a user by username\n"
         print "e.Get the id of post of user by username\n"
-        print "f.Get a list of people who have liked the recent post of own post\n"
-        print "g.Like the recent post of a user\n"
-        print "h.Get a list of comments on the recent post of own post\n"
-        print "i.Make a comment on the recent post of a user\n"
-        print "j.Delete negative comments from the recent post of a user\n"
-        print "k.Exit"
+        print "f.Get the recent media like by user\n "
+        print "g.Get a list of people who have liked the recent post of own post\n"
+        print "h.Like the recent post of a user\n"
+        print "i.Get a list of comments on the recent post of own post\n"
+        print "j.Make a comment on the recent post of a user\n"
+        print "k.Delete negative comments from the recent post of a user\n"
+        print "l.Iterate through the negative comments.\n"
+        print "m.Minimum like on post\n"
+        print "n.Exit\n"
 
         choice=raw_input("Enter you choice: ")
         if choice=="a":
@@ -252,23 +346,33 @@ def start_bot():
             insta_username = raw_input("Enter the username of the user: ")
             get_post_id(insta_username)
         elif choice == "f":
-            insta_username = raw_input("Enter the username of the user: ")
+            #insta_username = raw_input("Enter the username of the user: ")
+            recent_media_liked()
+        elif choice == "g":
+            insta_username = raw_input("Enter the user name of the user:")
             get_like_list(insta_username)
-        elif choice=="g":
-            insta_username = raw_input("Enter the username of the user: ")
-            like_a_post(insta_username)
         elif choice=="h":
             insta_username = raw_input("Enter the username of the user: ")
-            get_comment_list(insta_username)
+            like_a_post(insta_username)
         elif choice=="i":
             insta_username = raw_input("Enter the username of the user: ")
-            post_a_comment(insta_username)
+            get_comment_list(insta_username)
         elif choice=="j":
             insta_username = raw_input("Enter the username of the user: ")
-            delete_negative_comment(insta_username)
+            post_a_comment(insta_username)
         elif choice=="k":
+            insta_username = raw_input("Enter the username of the user: ")
+            delete_negative_comment(insta_username)
+        elif choice=="l":
+            insta_username = raw_input("Enter the username of the user: ")
+            media_id=get_post_id(insta_username)
+            iterate_through_negative_comments(media_id)
+        elif choice=="m":
+            insta_username = raw_input("Enter the username of the user: ")
+            min_likes_on_post(insta_username)
+        elif choice=="n":
             exit()
         else:
             print "wrong choice"
 
-start_bot()
+start_bot()                                                        #function calling
